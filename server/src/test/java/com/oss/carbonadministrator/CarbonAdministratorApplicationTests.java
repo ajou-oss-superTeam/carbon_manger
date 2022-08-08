0f118682322fc2537c326c97f26f9319cba414a1
DESKTOP-0RDFKIA\dnrla <dnrlalth@gmail.com>
2022-08-08 19:08:03 +0900

    [main update 0.2] 오류수정

---
+++ server/src/test/java/com/oss/carbonadministrator/CarbonAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss.carbonadministrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonAdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
