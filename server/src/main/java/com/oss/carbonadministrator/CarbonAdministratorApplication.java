fbfe224acabaad717737ab64c0dff32b6846083a
DJ-archive <ydajeong7@gmail.com>
2022-08-10 11:56:55 +0900

    style: checkstyle, github action
    체크스타일 indentation 수정, main githubaction 적용

--- server/src/main/java/com/oss/carbonadministrator/CarbonAdministratorApplication.java
+++ server/src/main/java/com/oss/carbonadministrator/CarbonAdministratorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarbonAdministratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarbonAdministratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonAdministratorApplication.class, args);
+    }
 }
