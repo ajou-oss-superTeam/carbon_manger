e2355acdc575c2bdbf4687a17037d16f618eb613
sean <tmddnr3163@gmail.com>
2022-08-21 15:29:17 +0900

    feat: pre loading 구현

--- client/App.js
+++ client/App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { Text, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset, useAssets } from 'expo-asset';
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [assets] = useAssets([require('./my-face.jpeg')]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require('./my-face.jpeg'),
+      'https://reactnative.dev/img/oss_logo.png',
+    ]);
+    await Promise.all([...fonts, ...images]);
+  };
+
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+
+  return <Text>We are done loading!</Text>;
+}
