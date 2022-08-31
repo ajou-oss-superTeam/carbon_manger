38904c6acd8ae76072253d4b2abe01d03991b3a8
DESKTOP-0RDFKIA\dnrla <dnrlalth@gmail.com>
2022-08-31 12:45:56 +0900

    [02_4] checkstyle 수정 -db변동으로 인한 test 수정

--- server/src/test/java/com/oss/carbonadministrator/CarbonAdministratorApplicationTests.java
+++ server/src/test/java/com/oss/carbonadministrator/CarbonAdministratorApplicationTests.java
@@ -21,13 +21,13 @@ class CarbonAdministratorApplicationTests {
 
 	@Test
 	void dbLinkTest(){
-		Optional<User> test = userRepository.findByEmail("test");
+		Optional<User> test = userRepository.findByEmail("dfldlk@gmail.com");
 		if(!test.isPresent()) {
 			fail("검색 결과 없어서 연결 테스트 불가");
 			return;
 		}
 		User user = test.get();
-		assertEquals("test", user.getEmail());
+		assertEquals("dfldlk@gmail.com", user.getEmail());
 	}
 
 }
