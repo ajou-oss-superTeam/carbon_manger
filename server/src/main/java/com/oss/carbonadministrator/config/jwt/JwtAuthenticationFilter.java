81520394e3794c5ea7744847bf8df627bdee64bc
DJ-archive <ydajeong7@gmail.com>
2022-09-02 03:12:18 +0900

    feat: 로그인 확인

--- server/src/main/java/com/oss/carbonadministrator/config/jwt/JwtAuthenticationFilter.java
+++ server/src/main/java/com/oss/carbonadministrator/config/jwt/JwtAuthenticationFilter.java
@@ -1,12 +1,14 @@
 package com.oss.carbonadministrator.config.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oss.carbonadministrator.config.security.CustomUserDetails;
 import com.oss.carbonadministrator.controller.request.LoginRequest;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,18 +30,26 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         // request에 있는 user의 email과 password를 파싱해서 자바 Object로 받기
         ObjectMapper om = new ObjectMapper();
-        LoginRequest loginRequestDto = null;
+        LoginRequest loginRequestDto;
 
         try {
-            loginRequestDto = om.readValue(request.getInputStream(), loginRequestDto.getClass());
+            loginRequestDto = om.readValue(request.getInputStream(), LoginRequest.class);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
 
         // 유저네임패스워드 토큰 생성
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+            loginRequestDto.getEmail(), loginRequestDto.getPassword());
+        log.info("JwtAuthenticationFilter : 토큰생성완료");
 
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
-        return super.attemptAuthentication(request, response);
+        // 로그인 확인
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        log.info("로그인 완료 됨. Authentication: {}", customUserDetails.getUsername());
+
+        return authentication;
     }
 }
