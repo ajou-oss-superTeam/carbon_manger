081ce3a0e23dace74d862346b20672dfbbcc4450
DJ-archive <ydajeong7@gmail.com>
2022-09-02 03:18:02 +0900

    feat: JWT 토큰 생성

--- server/src/main/java/com/oss/carbonadministrator/config/jwt/JwtAuthenticationFilter.java
+++ server/src/main/java/com/oss/carbonadministrator/config/jwt/JwtAuthenticationFilter.java
@@ -1,9 +1,14 @@
 package com.oss.carbonadministrator.config.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oss.carbonadministrator.config.security.CustomUserDetails;
 import com.oss.carbonadministrator.controller.request.LoginRequest;
 import java.io.IOException;
+import java.util.Date;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -51,5 +56,25 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         log.info("로그인 완료 됨. Authentication: {}", customUserDetails.getUsername());
 
         return authentication;
+    }
+
+    // attemptAuthentication 실행 후 인증이 정상적으로 되었으면 successfulAuthentication 함수 실행
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+        HttpServletResponse response, FilterChain chain, Authentication authResult)
+        throws IOException, ServletException {
+
+        CustomUserDetails customUserDetails = (CustomUserDetails) authResult.getPrincipal();
+
+        String jwtToken = JWT.create()
+            .withSubject(customUserDetails.getUsername())
+            .withExpiresAt(
+                new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME)
+            ) // 토큰 만료 시간
+            .withClaim("id", customUserDetails.getUser().getPid()) // 변경 가능
+            .withClaim("username", customUserDetails.getUser().getEmail()) // 변경 가능
+            .sign(Algorithm.HMAC512(JwtProperties.SECRET));
+
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
     }
 }
