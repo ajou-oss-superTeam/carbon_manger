93ca5afe109455bbb28b37b355b8e4f74583fb76
sean <tmddnr3163@gmail.com>
2022-10-15 16:57:05 +0900

    feat: 토큰 수정

--- client/api/index.js
+++ client/api/index.js
@@ -7,17 +7,13 @@ const API = {
   // 로그인
   async getLogin({ email, password }) {
     try {
-      const { data, headers } = await axios.post(`${host}/api/user/login`, {
+      const { data } = await axios.post(`${host}/api/user/login`, {
         email,
         password,
       });
 
       if (data.success) {
-        return {
-          user: data.data,
-          success: data.success,
-          token: headers.authorization,
-        };
+        return { user: data.data, success: data.success };
       } else {
         return { message: data.message, success: data.success };
       }
@@ -29,7 +25,7 @@ const API = {
   // 회원가입
   async getSignup({ email, nickname, password, province, city }) {
     try {
-      const { data, headers } = await axios.post(`${host}/api/user/signup`, {
+      const { data } = await axios.post(`${host}/api/user/signup`, {
         email,
         nickname,
         password,
@@ -37,14 +33,8 @@ const API = {
         city,
       });
 
-      console.log(headers);
-
       if (data.success) {
-        return {
-          user: data.data,
-          success: data.success,
-          token: headers.authorization,
-        };
+        return { user: data.data, success: data.success };
       } else {
         return { message: data.message, success: data.success };
       }
@@ -53,25 +43,16 @@ const API = {
     }
   },
 
-  // =======================================
-  // 전기 이미지 전송
-  async sendImg(email, uri, base, year, month, token) {
+  // 이미지 전송
+  async sendImg(email, uri, base, year, month) {
     try {
-      const { data } = await axios.post(
-        `${host}/api/image/electricity`,
-        {
-          email,
-          year,
-          month,
-          image: base,
-          uri,
-        },
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
+      const { data } = await axios.post(`${host}/api/image/electricity`, {
+        email,
+        year,
+        month,
+        image: base,
+        uri,
+      });
 
       if (data.success) {
         return { data: data.data, success: data.success };
@@ -84,19 +65,14 @@ const API = {
     }
   },
 
-  // 전기 사진 수정
-  async editImgInfo(id, numbers, token) {
+  // 사진 수정
+  async editImgInfo(id, numbers) {
     try {
       const { data } = await axios.put(
         `${host}/api/image/electricity/${id}/edit`,
         {
           id,
           ...numbers,
-        },
-        {
-          headers: {
-            Authorization: token,
-          },
         }
       );
 
@@ -110,130 +86,31 @@ const API = {
     }
   },
 
-  // 전기 숫자 전송
-  async sendNumber(email, year, month, numbers, token) {
+  // 숫자 전송
+  async sendNumber(email, year, month, numbers) {
+    try {
+      const { data } = await axios.post(`${host}/api/image/electricity/input`, {
+        email,
+        year,
+        month,
+        ...numbers,
+      });
+
+      if (data.success) {
+        return { data: data.data, success: data.success };
+      } else {
+        return { message: data.message, success: data.success };
+      }
+    } catch (err) {
+      console.error(err.response);
+    }
+  },
+
+  // 숫자 전송
+  async getGraph(email) {
     try {
       const { data } = await axios.post(
-        `${host}/api/image/electricity/input`,
-        {
-          email,
-          year,
-          month,
-          ...numbers,
-        },
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-
-      if (data.success) {
-        return { data: data.data, success: data.success };
-      } else {
-        return { message: data.message, success: data.success };
-      }
-    } catch (err) {
-      console.error(err.response);
-    }
-  },
-
-  // 전기 그래프
-  async getGraph(email, token) {
-    try {
-      const { data } = await axios.post(
-        `${host}/api/graph/electricity/fee?email=${email}`,
-        {},
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-
-      if (data.success) {
-        return { data: data.data, success: data.success };
-      } else {
-        return { message: data.message, success: data.success };
-      }
-    } catch (err) {
-      console.error(err.response);
-    }
-  },
-
-  // =======================================
-  // 가스 이미지 전송
-  async sendGasImg(email, uri, base, year, month, token) {
-    try {
-      const { data } = await axios.post(
-        `${host}/api/image/gas`,
-        {
-          email,
-          year,
-          month,
-          image: base,
-          uri,
-        },
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-
-      if (data.success) {
-        return { data: data.data, success: data.success };
-      } else {
-        return { message: data.message, success: data.success };
-      }
-    } catch (err) {
-      console.error(err.response);
-      return { message: '/api/image/gas 에러', success: false };
-    }
-  },
-
-  // 가스 사진 수정
-  async editGasImgInfo(id, numbers, token) {
-    try {
-      const { data } = await axios.put(
-        `${host}/api/image/gas/${id}/edit`,
-        {
-          id,
-          ...numbers,
-        },
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-
-      if (data.success) {
-        return { data: data.data, success: data.success };
-      } else {
-        return { message: data.message, success: data.success };
-      }
-    } catch (err) {
-      console.error(err.response);
-    }
-  },
-
-  // 가스 숫자 전송
-  async sendGasNumber(email, year, month, numbers, token) {
-    try {
-      const { data } = await axios.post(
-        `${host}/api/image/gas/input`,
-        {
-          email,
-          year,
-          month,
-          ...numbers,
-        },
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
+        `${host}/api/graph/electricity/fee?email=${email}`
       );
 
       if (data.success) {
