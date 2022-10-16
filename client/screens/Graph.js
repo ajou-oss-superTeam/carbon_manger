67daf8f8151a165ca93b28f394ebb7487167f67d
sean <tmddnr3163@gmail.com>
2022-10-16 16:22:18 +0900

    feat: 그래프 및 마이페이지 개발

--- client/screens/Graph.js
+++ client/screens/Graph.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Dimensions, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { LineChart } from 'react-native-chart-kit';
+import { LineChart, StackedBarChart } from 'react-native-chart-kit';
 import API from '../api/index';
 
 const Graph = () => {
@@ -24,16 +24,25 @@ const Graph = () => {
 
     // 그패프 데이터
     if (parseUser?.user?.email) {
-      const { data, message, success } = await API.getGraph(
-        parseUser?.user?.email,
-        parseToken
-      );
+      // const { data, message, success } = await API.getGraph(
+      //   parseUser?.user?.email,
+      //   parseToken
+      // );
 
-      if (success) {
-        setGraphData(data);
-      } else {
-        Alert.alert(message);
-      }
+      // if (success) {
+      //   setGraphData(data);
+      // } else {
+      //   Alert.alert(message);
+      // }
+      setGraphData({
+        labels: ['21/12', '22/01', '22/03'],
+        legend: ['전기', '가스', '수도'],
+        datasets: [
+          [60, 20, ''],
+          [10, 30, ''],
+          ['', 50, ''],
+        ],
+      });
     }
   };
 
@@ -41,41 +50,84 @@ const Graph = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>
-          {nickname || '닉네임'}님의 월별 사용량
+          {nickname || '닉네임'}님의 탄소 배출량
         </Text>
       </View>
       <View style={styles.middle}>
         {graphData && (
-          <LineChart
+          // <LineChart
+          //   data={{
+          //     labels: graphData.labels,
+          //     datasets: [
+          //       {
+          //         data: graphData.datasets.userData,
+          //         color: (opacity = 1) => 'rgba(58, 143, 255, 1)',
+          //       },
+          //       {
+          //         data: graphData.datasets.averageData,
+          //         color: (opacity = 1) => 'rgba(0, 255, 255, 1)',
+          //       },
+          //       { data: [0], withwithDots: false },
+          //     ],
+          //   }}
+          //   width={Dimensions.get('window').width - 20} // from react-native
+          //   height={400}
+          //   chartConfig={{
+          //     backgroundColor: '#1cc910',
+          //     backgroundGradientFrom: '#eff3ff',
+          //     backgroundGradientTo: '#efefef',
+          //     decimalPlaces: 2, // optional, defaults to 2dp
+          //     color: (opacity = 1) => `black`,
+          //     style: {
+          //       borderRadius: 16,
+          //     },
+          //   }}
+          //   bezier
+          //   style={{
+          //     borderRadius: 15,
+          //   }}
+          // />
+          // <StackedBarChart
+          //   data={graphData}
+          //   width={Dimensions.get('window').width - 20} // from react-native
+          //   height={400}
+          //   chartConfig={{
+          //     backgroundColor: '#1cc910',
+          //     backgroundGradientFrom: '#eff3ff',
+          //     backgroundGradientTo: '#efefef',
+          //     decimalPlaces: 2, // optional, defaults to 2dp
+          //     color: (opacity = 1) => `black`,
+          //     style: {
+          //       borderRadius: 16,
+          //     },
+          //   }}
+          //   bezier
+          //   style={{
+          //     borderRadius: 15,
+          //   }}
+          // />
+          <StackedBarChart
             data={{
               labels: graphData.labels,
-              datasets: [
-                {
-                  data: graphData.datasets.userData,
-                  color: (opacity = 1) => 'rgba(58, 143, 255, 1)',
-                },
-                {
-                  data: graphData.datasets.averageData,
-                  color: (opacity = 1) => 'rgba(0, 255, 255, 1)',
-                },
-                { data: [0], withwithDots: false },
-              ],
+              legend: graphData.legend,
+              data: graphData.datasets,
+              barColors: ['#dfe4ea', '#ced6e0', '#a4b0be'],
             }}
-            width={Dimensions.get('window').width - 20} // from react-native
+            width={Dimensions.get('window').width - 16}
             height={400}
             chartConfig={{
               backgroundColor: '#1cc910',
               backgroundGradientFrom: '#eff3ff',
               backgroundGradientTo: '#efefef',
-              decimalPlaces: 2, // optional, defaults to 2dp
-              color: (opacity = 1) => `black`,
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
               style: {
                 borderRadius: 16,
               },
             }}
-            bezier
             style={{
-              borderRadius: 15,
+              marginVertical: 8,
+              borderRadius: 16,
             }}
           />
         )}
